cbdee41b8ab8182aea279da71da7300d57f7631c
Snehal Kolhe <snehalkolhe2628@gmail.com>
2025-11-29 14:47:42 +0530

    snake algorithm

--- script.js
+++ script.js
@@ -8,20 +8,18 @@ const rows = Math.floor(board.clientHeight / blockHeight);
 
 const blocks = {};
 const snake = [{
-    x: 1, y: 3
+    x:1, y:3
 }, {
-    x: 1, y: 4
+    x:1, y:4
 }, {
-    x: 1, y: 5
+    x:1, y:5
 }
 
 ]
 
-// for(let i= 0; i<rows * cols; i++){
-//     const block = document.createElement('div');
-//     block.classList.add('block');
-//     board.appendChild(block);
-// }
+let direction = 'down';
+
+
  for(let row= 0; row<rows; row++){
     for(let col=0;col<cols; col++){
         const block = document.createElement('div');
@@ -40,3 +38,53 @@ const snake = [{
     
  })
  }
+
+ setInterval( () => {
+
+    let head = null;
+
+    if(direction === 'left'){
+        head = {
+            x: snake[0].x,
+            y: snake[0].y - 1
+        }
+
+    } else if(direction === 'right'){
+        head = {
+            x: snake[0].x,
+            y: snake[0].y + 1
+        }
+    }else if(direction === 'up'){
+        head = {
+            x: snake[0].x - 1,
+            y: snake[0].y
+        }
+    }else if(direction === 'down'){
+        head = {
+            x: snake[0].x + 1,
+            y: snake[0].y
+        }
+    }
+
+    snake.forEach( segment => {
+        blocks[ `${segment.x},${segment.y}` ].classList.remove('fill');
+    })  
+
+    snake.unshift(head);
+    snake.pop();
+
+    render()
+    }, 300);    
+
+    addEventListener('keydown', (event) => {
+        console.log(event.key)
+        if(event.key === 'ArrowLeft'){
+            direction = 'left';
+        } else if(event.key === 'ArrowRight'){
+            direction = 'right';
+        } else if(event.key === 'ArrowUp'){
+            direction = 'up';
+        } else if(event.key === 'ArrowDown'){
+            direction = 'down';
+        }   
+    })
